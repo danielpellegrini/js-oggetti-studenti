de6cf9c906ea481a6633c7d61d7aea69b4b2e75d
Daniel <danielpellegrini87@gmail.com>
2021-01-18 15:43:36 +0100

    'for - in' inside standard for cycle added

--- js/script.js
+++ js/script.js
@@ -2,15 +2,54 @@
 // nome, cognome e età. Stampare a schermo attraverso il for tutte le proprietà.
 
 var student = {
-  'name' : 'John',
-  'surname' : 'Doe',
-  'age' : 20 + ' years old',
+  'Name': 'John',
+  'Surname': 'Doe',
+  'Age': 20 + ' years old',
 }
 
 for (var key in student) {
   console.log(key + ': ' + student[key])
 }
 
+console.log('___________________________');
+
 
 // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+
+var school = {
+  'students': [{
+      'Name': 'John',
+      'Surname': 'Doe',
+    },
+    {
+      'Name': 'Jane',
+      'Surname': 'Doe',
+    },
+    {
+      'Name': 'Desmond',
+      'Surname': 'Eagle',
+    },
+    {
+      'Name': 'Dianne',
+      'Surname': 'Ameter',
+    },
+    {
+      'Name': 'Max',
+      'Surname': 'Conversion',
+    },
+  ]
+}
+
+
+for (var i = 0; i < school.students.length; i++) {
+  console.log('STUDENT N. ' + (i + 1));
+
+  var guy = school.students[i];
+
+  for (var key in guy) {
+    console.log(key + ': ' + guy[key]);
+
+  }
+}
+
 // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
